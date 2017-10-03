68c6dd2985e94309ef816c806fb5687f4327eb1b
Anton Varlamov <vad@barl.ru>
2017-10-03 17:26:52 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { createStore, compose, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import reducer from '../reducers'
+import root from '../sagas/root'
+
+import t1 from './t1'
+import t2 from './t2'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+    reducer,
+    compose(
+        applyMiddleware(sagaMiddleware),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+)
+
+sagaMiddleware.run(root)
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={t1}/>
+                        <Route exact path='/edit' component={t2}/>
+                    </Switch>
+                </Router>
+            </Provider>
+        )
+    }
+}
+
+export default App
