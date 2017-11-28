64e21e8b0842e81c75a2b62adb09296587db23c8
vad <vad@barl.ru>
2017-11-28 11:51:45 +0300

    CSS - Grid - версия объединения ячеек (работает и можно верстать)

--- src/components/experience/dayComponents/DayDirectionInLine.jsx
+++ src/components/experience/dayComponents/DayDirectionInLine.jsx
@@ -11,6 +11,78 @@ class DayDirectionLine extends Component {
         }
         if (subjects === undefined) {
             if (week === '-') {
+                console.log('HARD COORDINATE', this.props.index, 2)
+                return (
+                    <div 
+                        style={{
+                            width: '100%', 
+                            backgroundColor: 'red',
+                            gridColumn: this.props.index + 1,
+                            gridRow: '1 / 3'
+                        }}> 
+                        Пусто 
+                    </div>
+                )
+            } return null
+        }
+        if (subjects['-']
+            && subjects['+']
+            && subjects['-'].name == subjects['+'].name && colSpan['-'] === colSpan['+']) {
+            if (week === '-') {
+                return (
+                    <div rowSpan='2' colSpan={colSpan['-']} 
+                        style={{
+                            width: '100%', 
+                            backgroundColor: '#d0f0c0',
+                            gridColumn: this.props.index + 1 + ' / ' + (this.props.index + colSpan['-'] + 1),
+                            gridRow: '1 / 3'
+                        }}> 
+                        {subjects['-'].name} 
+                    </div>
+                )
+            } return null
+        }
+        
+        if (!subjects[week]) {
+            return (
+                <div 
+                    style={{
+                        width: '100%', 
+                        backgroundColor: '#ffcf48',
+                        gridRow: week === '+' ? '2 / 3' : '1 / 2',
+                        gridColumn: this.props.index + 1 + ' / ' + (this.props.index + colSpan[week] + 1)
+                    }}
+                >
+                    Нет предмета 
+                </div>
+            )
+        }
+        // console.log('странный случай', subjects['-'].name, subjects['+'].name, week)
+        return (
+            <div 
+                style={{
+                    width: '100%', 
+                    backgroundColor: '#48d1cc',
+                    gridRow: week === '+' ? '2 / 3' : '1 / 2',
+                    gridColumn: this.props.index + 1 + ' / ' + (this.props.index + colSpan[week] + 1)
+                }}
+            > 
+                {subjects[week].name + week} 
+            </div>
+        )
+    }
+}
+
+/*
+render() {
+        // console.log('data', this.props.data)
+        const {data: subjects, week, colSpan} = this.props
+        // console.log('colspan', colSpan, colSpan['-'] === colSpan['+'])
+        if (colSpan[week] === 0) {
+            return null
+        }
+        if (subjects === undefined) {
+            if (week === '-') {
                 return (
                     <td rowSpan='2' style={{width: '20%', backgroundColor: 'red'}}> Пусто </td>
                 )
@@ -20,7 +92,6 @@ class DayDirectionLine extends Component {
             && subjects['+']
             && subjects['-'].name == subjects['+'].name && colSpan['-'] === colSpan['+']) {
             if (week === '-') {
-                console.log('same subjects', subjects['-'].name, subjects['+'].name)
                 return (
                     <td rowSpan='2' colSpan={colSpan['-']} style={{width: '20%', backgroundColor: '#d0f0c0'}}> {subjects['-'].name} </td>
                 )
@@ -36,12 +107,13 @@ class DayDirectionLine extends Component {
             <td style={{width: '20%', backgroundColor: '#48d1cc'}} colSpan={colSpan[week]} > {subjects[week].name + week} </td>
         )
     }
-}
+*/
 
 DayDirectionLine.propTypes = {
     data: PropTypes.object,
     week: PropTypes.string,
-    colSpan: PropTypes.object
+    colSpan: PropTypes.object,
+    index: PropTypes.number
 }
 
 export default DayDirectionLine
--- src/components/experience/dayComponents/DayLine.jsx
+++ src/components/experience/dayComponents/DayLine.jsx
@@ -3,54 +3,46 @@ import PropTypes from 'prop-types'
 import DayDirectionInLine from './DayDirectionInLine'
 
 class DayLine extends Component {
+    componentDidMount() {
+        if (this.props.directions) {
+            const cellWidth = 100 / this.props.directions.length
+            this.setState({wrapperStyle: Object.assign(
+                {}, 
+                this.state.wrapperStyle, 
+                {gridTemplateColumns: 'repeat(' + this.props.directions.length + ', ' + cellWidth + '%)'})})
+            // directions.map(direction => cellWidth + '%').join(' ')
+            console.log('hell', this.wrapperStyle)
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        if (this.props.directions !== nextProps.directions) {
+            const cellWidth = 100 / nextProps.directions.length
+            this.setState({wrapperStyle: Object.assign(
+                {}, 
+                this.state.wrapperStyle, 
+                {gridTemplateColumns: 'repeat(' + nextProps.directions.length + ', ' + cellWidth + '%)'})})
+            // directions.map(direction => cellWidth + '%').join(' ')
+            console.log('hell', this.wrapperStyle)
+        }
+    }
+
     constructor(props) {
         super(props)
-        this.subjectsColumnSpan = {}     
-    }
-
-    /*
-    calculateColSpan = (directionIndex, week) => {
-        const {timetable, directions} = this.props
-        if (!timeTable[directions[directionIndex]] || !timeTable[directions[directionIndex]][week]) {return 1}
-        let prevCounter = []
-        for (let i = 0; i < directionIndex; i++) {
-            prevCounter.push(this.calculateColSpan(i, week))
-        }
-        let checkCount = 1
-        if (prevCounter[prevCounter.length - 1] > checkCount) {return 0}
-        for (let i = directionIndex + 1; i < directions.length; i++) {
-            if (timeTable[directions[i]]
-                && timeTable[directions[i]][week]
-                && timeTable[directions[i]][week].name === timeTable[directions[directionIndex]][week].name
-            ) {
-                counter ++
-            } else {return counter}
-        }
-    }
-    */
-    render() {
-        /*
-        console.log('directions: ', this.props.directions)
-        console.log('items: ', this.props.timeTable)
-        const minusLine = []
-        const plusLine  = []
-        const bothLine  = []
-        // 1) Объединение по вертикали
-        this.props.directions.map((direction, key) => {
-            if (this.props.timeTable[direction.abbr_key]
-                && this.props.timeTable[direction.abbr_key]['-']
-                && this.props.timeTable[direction.abbr_key]['+']
-                && this.props.timeTable[direction.abbr_key]['-'][0].name === this.props.timeTable[direction.abbr_key]['+'][0].name) {
-                bothLine.push({ subjects: this.props.timeTable[direction.abbr_key]['-'], heightFlex: 2})
-            } else {
-                bothLine.push({ subjects: this.props.timeTable[direction.abbr_key], heightFlex: 1})
+        this.state = {
+            wrapperStyle: {
+                display: 'grid',
+                gridTemplateColumns: '100px 100px 100px',
+                gridTemplateRows: '50px 50px',
+                backgroundColor: 'green',
+                width: '100%'
             }
-        })
-        */
+        }
+        this.subjectsColumnSpan = {} 
+    }
 
-        // console.log('объединение: ', bothLine)
+    render() {
         const {timeTable, directions} = this.props
-        console.log('timetable', timeTable)
+        // console.log('timetable', timeTable)
         directions.map((direction, key) => {
             const minusSpan = 1
             const plusSpan = 1
@@ -58,7 +50,6 @@ class DayLine extends Component {
         })
 
         directions.map((direction, key) => {
-            // this.subjectsColumnSpan[direction.abbr_key] = {'-': minusSpan, '+': plusSpan}
             let stopMinus = false
             let stopPlus = false
             for (let i = key + 1; i < directions.length; i++) {
@@ -79,7 +70,6 @@ class DayLine extends Component {
                 && this.subjectsColumnSpan[direction.abbr_key]['+'] !== 0
                 && !stopPlus
                 ) {
-                    console.log(timeTable[someDirection.abbr_key]['+'].name, timeTable[direction.abbr_key]['+'].name, someDirection.abbr_key, direction.abbr_key)
                     this.subjectsColumnSpan[direction.abbr_key]['+'] += 1
                     this.subjectsColumnSpan[someDirection.abbr_key]['+'] = 0
                 } else {stopPlus = true}
@@ -90,7 +80,32 @@ class DayLine extends Component {
         console.log('time', this.props.time)
         console.log('timetable', this.props.timeTable)
         return (
-            <table className='table is-bordered is-fullwidth is-hoverable' style={{marginBottom: '0'}}> 
+            <div style={this.state.wrapperStyle}>
+                {directions.map((direction, key) => (
+                    <DayDirectionInLine
+                        key={key}
+                        data={timeTable[direction.abbr_key]}
+                        week={'-'}
+                        colSpan={this.subjectsColumnSpan[direction.abbr_key]}
+                        index={key}
+                    />
+                ))}        
+                {directions.map((direction, key) => (
+                    <DayDirectionInLine
+                        key={key}
+                        data={timeTable[direction.abbr_key]}
+                        week={'+'}
+                        colSpan={this.subjectsColumnSpan[direction.abbr_key]}
+                        index={key}
+                    />
+                ))}
+            </div>      
+        )
+    }
+}
+
+/*
+<table className='table is-bordered is-fullwidth is-hoverable' style={{marginBottom: '0'}}> 
                 <tbody>
                     <tr>
                         {directions.map((direction, key) => (
@@ -113,69 +128,7 @@ class DayLine extends Component {
                         ))}
                     </tr>
                 </tbody>
-            </table>           
-        )
-    }
-}
-
-/*
-<div style={oneLineContainerStyle}>
-                {bothLine.map((items, key)=>(
-                    items.heightFlex === 1 ? (
-                        items.subjects ? (
-                            <div key={key} style={containerStyle}>
-                                <div style={itemStyle}>
-                                    {items.subjects['-'] && items.subjects['-'].map((subject, skey) => (
-                                        <div key={skey} style={itemStyle}>
-                                            {subject.name + ' -'}
-                                        </div>
-                                    ))}
-                                </div>
-                                <div style={itemStyle}>
-                                    {items.subjects['+'] && items.subjects['+'].map((subject, skey) => (
-                                        <div key={skey} style={itemStyle}>
-                                            {subject.name + ' +'}
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-                        ) : (
-                            <div key={key} style={itemStyle} />
-                        )
-                    ) : (
-                        <div key={key} style={containerStyle}>
-                            {items.subjects.map((subject, skey) => (
-                                <div key={skey} style={itemStyle}>
-                                    {subject.name}
-                                </div>
-                            ))}
-                        </div>
-                    )
-                ))}
-            </div>
-*/
-
-/*
-<div style={containerStyle}>
-                <div style={oneLineContainerStyle}>
-                    {this.props.directions.map((direction, key) => (
-                        <div key={key} style={itemStyle}> 
-                            {this.props.timeTable[direction.abbr_key] 
-                                && this.props.timeTable[direction.abbr_key]['-']
-                                && this.props.timeTable[direction.abbr_key]['-'][0].name || 'нет -'} 
-                        </div>
-                    ))}
-                </div>
-                <div style={oneLineContainerStyle}>
-                    {this.props.directions.map((direction, key) => (
-                        <div key={key} style={itemStyle}> 
-                            {this.props.timeTable[direction.abbr_key] 
-                            && this.props.timeTable[direction.abbr_key]['+']
-                            && this.props.timeTable[direction.abbr_key]['+'][0].name || 'нет +'} 
-                        </div>
-                    ))}
-                </div>
-            </div>
+            </table>
 */
 
 DayLine.propTypes = {
